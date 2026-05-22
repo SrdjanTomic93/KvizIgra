26e1ec9b9a2bff161e0c7c0ded9cd637be8c86a7
SrdjanTomic93 <srdjantomic93.com>
2026-05-22 13:05:35 +0200

    Port promenjen na 8080

--- server.js
+++ server.js
@@ -310,7 +310,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server radi na portu ${PORT}`);
 });
